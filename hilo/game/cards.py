938adcbb92f74fad08c4dc7691b7bccaf66a3ff3
clarkscoberly <cob18002@byui.edu>
2022-03-03 12:25:27 -0700

    Worked on fixing calling the draw method, other fixes included a check to see if the code is being run for the first time or not, added a previous card variable -- began the is_higher_lower method,  added comments.

--- hilo/game/cards.py
+++ hilo/game/cards.py
@@ -1,14 +1,21 @@
 import random
 
+# Purpose: Create cards and check values.
 class Cards:
 
-    def __init__(self, user_guess):
-        card_list = self.create_deck
-        pass
+    def __init__(self):
+        # Stores previous card
+        self.previous_card = None
+        
+        # Stores current card which will be displayed to the user
+        self.card = [0, 'Wowza']
+
+        # Variable that checks to see if the game is starting up or has already been played once.
+        self.second_time = 0
 
 
-    def create_deck():
-        # Create  a list of cards and store 
+    # Create a list of cards and store
+    def create_deck(self):
         card_list = []
         for suit in ['Spade', 'Club', 'Diamond', 'Heart']:
             for number in range(1, 14):
@@ -18,20 +25,38 @@ class Cards:
         return card_list
 
     def draw(self):
-        card_list = self.create_deck()
-        card = card_list[random.randint(0, len(card_list) - 1)]
-        print(card[0])
+        # If the game is starting up for the first time create a deck of cards
+        if not self.second_time:
+            card_list = self.create_deck()
+            assert len(card_list) == 52, 'For loops are off if this fires'
+            self.second_time += 1
+            print('\n\n n \n\n')
 
 
-    def is_higher(card):
-        if 1-6 > 7:
+        # Keeps track of the previous card 
+        self.previous_card = self.card[0]
+        
+        # Choose a random card out of the 52 that have been created within the deck.
+        self.card = card_list[random.randint(0, len(card_list) - 1)]
+        
+        
+        return self.card
+
+    # Check user guess and return if the user was correct or not.
+    def is_higher_or_lower(self, card, user_guess):
+        if user_guess.lower() == 'h':
+            
             points += 100
         else:
-            points -=75
+            pass
         
         # user_guess > card
         print('it lives')
     pass
 
-    def is_lower():
-        pass
+
+
+# Currently the code below this makes it so that a drawn card is actually retrieved
+player1 = Cards()
+print('\n\n', player1.draw(), ' -- The previous is the card drawn \n\n')
+print(player1.previous_card)
