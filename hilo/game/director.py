84a14469ed67a695d7af3fc427d0104ca8a0fc28
djsevy <dallinsevy@gmail.com>
2022-03-03 17:36:56 -0700

    added  to game start, update game, output methods.

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -16,20 +16,33 @@ class Director:
     def game_start(self):
         
         # create instance of card class to show first card
-
         
+        # format of first card [number, suit]
+        
+        first_card = Cards.draw()
+        print(f'the first card is{first_card}')
         while self.is_playing == True:
+            
+            self.get_input()
+            Cards.draw()
+            self.update_game()
+            self.output()
 
-            pass
     
     # function to get input
     def get_input(self):
-        pass
+        self.user_guess = input('Guess higher or lower. (h/l)')
 
-    # function to update game
+    # function to compare user input, draw cards, and apply points.
     def update_game(self):
-        pass
+        Cards.is_higher_or_lower()
+        
+        """the mothod for points will be in the cards class, right?"""
+        # Cards.points()
+
 
     #function to show output
     def output(self):
-        pass    
+       
+       #for when we have the points method up
+        print('Your total points are ')    
