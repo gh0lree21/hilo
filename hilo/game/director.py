1726cb6dd4a7cfe4b802c9bc34095b266e580135
Grant Holley <grant.holley98@gmail.com>
2022-03-04 12:15:53 -0700

    Changes to output and using cards in Director

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -2,7 +2,7 @@ from game.cards import Cards
 
 
 class Director:
-    # controlls input, output and player turn.
+    # controls input, output and player turn.
 
     
     # defining class attributes
@@ -10,7 +10,7 @@ class Director:
         self.total_points = 300
         self.is_playing = True
         self.user_guess = ''
-        self.player = Cards()
+        self.cards = Cards()
     
     # function for game loop
     def game_start(self):
@@ -19,30 +19,34 @@ class Director:
         
         # format of first card [number, suit]
         
-        first_card = self.player.draw()
-        print(f'the first card is{first_card}')
+        first_card = self.cards.draw()
+        print(f'\nThe first card is {first_card[0]}, {first_card[1]}')
         while self.is_playing == True:
             
             self.get_input()
-            self.player.draw()
+            self.current_card = self.cards.draw()
             self.update_game()
             self.output()
 
     
     # function to get input
     def get_input(self):
-        self.user_guess = input('Guess higher or lower. (h/l)')
+        self.user_guess = input('Guess higher or lower. (h/l): ')
 
     # function to compare user input, draw cards, and apply points.
     def update_game(self):
-        give_points = self.player.is_higher_or_lower(self.user_guess)
+        give_points = self.cards.is_higher_or_lower(self.user_guess)
         
-        if give_points:
+
+        if give_points == True:
             self.total_points += 100
         else:
             self.total_points -= 75
+
+        if self.total_points <= 0:
+            self.output()
         
-        """the mothod for points will be in the cards class, right?"""
+        """the method for points will be in the cards class, right?"""
         # Cards.points()
 
 
@@ -50,4 +54,10 @@ class Director:
     def output(self):
        
        #for when we have the points method up
-        print(f'Your total points are{self.total_points}')    
+        if self.total_points <= 0:
+            print(f'\nYour total points are {self.total_points}')
+            print('Game Over!')  
+            self.is_playing = False
+        else:
+            print(f'\nYour total points are {self.total_points}') 
+            print(f'The next card was: {self.cards.draw()}')
